d1578494e21f67ec5479361ece4d22f683e2bd41
Rafael Miazaki <rafael.miazaki@globalhitss.com.br>
2019-06-24 06:11:02 -0300

    changing title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AWS summit 2019!', success: req.session.success, errors: req.session.errors });
+  res.render('index', { title: 'Claro', success: req.session.success, errors: req.session.errors });
   req.session.errors = null;
 });
 
